a523d71e2f2f43099ce351d60343f21cf3a1cd04
destinybravos <destinybravos@gmail.com>
2024-10-31 14:39:38 +0100

    Project Setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{html,js,css}", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
